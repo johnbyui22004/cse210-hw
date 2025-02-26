10600d49c5bdd95a922c653d6fe3fc5595b294d6
johnbyui22004 <johnbrocksman@gmail.com>
2025-02-26 11:37:59 -0700

    changing it to be camel case

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string firstname=Console.ReadLine();
+        string firstName=Console.ReadLine();
         Console.Write("What is your last name? ");
-        string lastname=Console.ReadLine();
+        string lastName=Console.ReadLine();
         Console.WriteLine();
         Console.WriteLine($"Your name is: {lastname}, {firstname} {lastname}");
     }
