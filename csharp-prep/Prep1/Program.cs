f5e5299d0113f8881019505f7234dedc5bdccf5b
johnbyui22004 <johnbrocksman@gmail.com>
2025-02-26 11:50:27 -0700

    fixed it

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -9,6 +9,6 @@ class Program
         Console.Write("What is your last name? ");
         string lastName=Console.ReadLine();
         Console.WriteLine();
-        Console.WriteLine($"Your name is: {lastname}, {firstname} {lastname}");
+        Console.WriteLine($"Your name is: {lastName}, {firstName} {lastName}");
     }
 }
