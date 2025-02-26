f979af302cad39b4d8c4f16677e6ba84276d468c
johnbyui22004 <johnbrocksman@gmail.com>
2025-02-26 12:16:43 -0700

    first save incase

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeAnswer = Console.ReadLine();
+        int gradePercentage = int.Parse(gradeAnswer);
+
+        string letter=" ";
+
+        if (gradePercentage >= 90)
+        {
+            letter="A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter="B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter="C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter="D";
+        }
+        else 
+        {
+            letter="F";
+        }
+        Console.WriteLine($"Your grade is {letter}");
+        if (gradePercentage>=70)
+        {
+            Console.WriteLine("Congratulations you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You've got it next time");
+        }
     }
 }
