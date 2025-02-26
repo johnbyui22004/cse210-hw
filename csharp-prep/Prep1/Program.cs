35241e4b2ef82f0d8e95d04153f170eebaf8eb90
johnbyui22004 <johnbrocksman@gmail.com>
2025-02-26 11:22:59 -0700

    fixed it to be like the assignment wanted

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,11 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
-        Console.Write("Please enter your name: ");
+        Console.Write("What is your first name? ");
         string firstname=Console.ReadLine();
-        Console.Write("Please enter your name: ");
+        Console.Write("What is your last name? ");
         string lastname=Console.ReadLine();
+        Console.WriteLine();
         Console.WriteLine($"Your name is: {lastname}, {firstname} {lastname}");
     }
 }
