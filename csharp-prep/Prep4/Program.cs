eefa1bf95e4ed697b9f51adbf83c544814587d60
johnbyui22004 <johnbrocksman@gmail.com>
2025-03-03 13:06:36 -0700

    this was an easy one

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int userInput = -1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            
+            Console.Write("Enter number:");
+            userInput = int.Parse(Console.ReadLine());
+            if(userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+           
+        //    Could also just use .sum
+        }while(userInput != 0);
+
+        int sum = 0;
+        foreach(int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average =((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach(int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
+
+  
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,50 +1,31 @@
 using System;
-using System.Security.Cryptography.X509Certificates;
 
+class Circle
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public double GetArea()
+    {
+        double area = Math.PI * radius * radius;
+        return area;
+    }
+}
 class Program
 {
-    public static int AddNumbe
     static void Main(string[] args)
     {
-/*         for (int i = 0; i < 20; i++)
-        {
-            Console.WriteLine($"{i}: Bob");
+        // double x=10;
+        Circle myCircle=new Circle(10);
 
-        }
- */
+        Console.WriteLine($"The area of the circle is: {myCircle.GetArea()}");
 
-        // list and string[]
-/*         List<int> numbers = new List<int>{1,2,3,4,5,6,7,8};
-        numbers.Add(10);
-
-        foreach(int x in numbers)
-        {
-            Console.WriteLine($"{x}");
-        }
-/*  */
-       /*  bool correctInput;
-        do
-        {
-            Console.Write("Please enter your age: ");
-            int age = int.Parse(Console.ReadLine());
-            if (age >= 0 && age < 121)
-            {
-                Console.WriteLine($"Your age is: {age}");
-                correctInput = true;
-            }
-            else
-            correctInput=false;
-        } while(!correctInput); */
-    
-/*         Random newRandomNumber=new Random();
-        for(int i = 0; i < 100; i++)
-        {
-            int number=newRandomNumber.Next(1, 100);
-            Console.WriteLine($"{i}; {number}");
-        } */
-
-        
     }
+    
 
     
 }
