c4352d94865e6c669b80925008622986e3ad7c01
johnbyui22004 <johnbrocksman@gmail.com>
2025-03-24 16:10:38 -0600

    made it take some time for the menu to appear so that the end message can be seen

--- prove/Develop04/Menu.cs
+++ prove/Develop04/Menu.cs
@@ -2,43 +2,47 @@ class Menu
 {
     private int _selectedNumber;
 
-    private Menu _menu; // Reference to Menu
-
-  
-
     public void DisplayMenu()
     {
+        Thread.Sleep(5000); // Delay for better user experience
         Console.Clear();
         Console.WriteLine("Menu options:");
-        
         Console.WriteLine(" 1. Start Breathing activity");
         Console.WriteLine(" 2. Start Reflecting activity");
         Console.WriteLine(" 3. Start Listing activity");
         Console.WriteLine(" 4. Exit");
         Console.Write("Please select an option: ");
-        _selectedNumber = int.Parse(Console.ReadLine());
+        
+        if (!int.TryParse(Console.ReadLine(), out _selectedNumber))
+        {
+            Console.WriteLine("ERROR: Please enter a valid number.");
+            DisplayMenu();
+            return;
+        }
 
         switch (_selectedNumber)
         {
             case 1:
                 Console.Clear();
-                BreathingActivity breathingActivity = new BreathingActivity();
+                BreathingActivity breathingActivity = new BreathingActivity(this); // Pass `this` (Menu instance)
                 breathingActivity.StartBreathingActivity();
                 break;
             case 2:
-
+                Console.Clear();
+                Console.WriteLine("Reflecting activity started");
                 break;
             case 3:
+                Console.Clear();
                 Console.WriteLine("Listing activity started");
                 break;
             case 4:
-                Console.WriteLine("Exiting");
-                break;
+                Console.Clear();
+                Console.WriteLine("Exiting...");
+                return;
             default:
-                Console.WriteLine("ERROR: Please enter an Integer from 1-4");
+                Console.WriteLine("ERROR: Please enter a number from 1-4.");
+                DisplayMenu();
                 break;
         }
     }
-
-
 }
